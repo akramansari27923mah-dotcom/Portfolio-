7920437b8733a487e40474307738584c06637511
Akram Ansari <akramansari27923mah@gmail.com>
2026-01-24 03:33:57 -0500

    3.0.6

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,18 +14,18 @@ const Project = ({ Image, title, desc, tag1, tag2, tag3, link, key }) => {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
           viewport={{ once: true }}
-          className="card flex flex-col backdrop-blur-sm bg-black/20 justify-center lg:gap-y-0  items-center gap-x-10 border border-gray-700 rounded-2xl overflow-hidden">
+          className="card flex flex-col backdrop-blur-sm bg-black/20 justify-center lg:gap-y-0  items-center gap-x-10 border border-gray-700 rounded-2xl overflow-hidden w-90 md:w-110">
 
           <div>
 
             <a target="_blank" href={link}>
-              <img className='w-110 h-45 object-cover' src={Image} alt={title} />
+              <img className='md:w-110 w-90  h-45 object-cover' src={Image} alt={title} />
             </a>
 
           </div>
 
           {/* content */}
-          <div className="flex flex-col justify-center gap-y-3 mx-2 my-3  ">
+          <div className="flex flex-col justify-center gap-y-3 md:mx-2 my-3 px-2 ">
 
             <h1
 
@@ -36,7 +36,7 @@ const Project = ({ Image, title, desc, tag1, tag2, tag3, link, key }) => {
 
             <p
 
-              className='text-gray-400 max-w-sm text-sm sm:text-base'>
+              className='text-gray-400 max-w-lg text-sm sm:text-base'>
               {desc.slice(0,130)}...
 
             </p>
@@ -45,7 +45,7 @@ const Project = ({ Image, title, desc, tag1, tag2, tag3, link, key }) => {
 
               <p
 
-                className='py-1 text-sm px-3 lg:px-5 rounded-xl bg-black/70 '
+                className='py-1 text-sm px-2 lg:px-5 rounded-xl bg-black/70 '
                 title={tag1}>
                 {tag1}
 
@@ -99,7 +99,7 @@ const Project = ({ Image, title, desc, tag1, tag2, tag3, link, key }) => {
               }
               {
                 tag3 === 'Firebase' ? <p
-                  className='py-1 text-sm px-3 rounded-xl bg-black/70 '
+                  className='py-1 text-sm px-2 rounded-xl bg-black/70 '
                   title={tag3}>
                   {tag3}
                 </p> :
@@ -111,7 +111,7 @@ const Project = ({ Image, title, desc, tag1, tag2, tag3, link, key }) => {
                 target='_blank'
                 title='Visit Project'
                 href={link}>
-                Visit Project
+                Visit ✨
               </a>
 
             </div>
